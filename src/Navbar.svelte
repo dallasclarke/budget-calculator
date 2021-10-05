44beb2d0a9f51965e6c4a3aa080d1ec80846cf86
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-10-04 21:07:48 -0700

    Created Navbar component and added in global styling.

---
+++ src/Navbar.svelte
@@ -0,0 +1,8 @@
+<nav class="nav">
+  <div class="nav-center">
+    <h1 class="nav-title">Budget Calculator</h1>
+    <button class="nav-btn" type="button">
+      <i class="far fa-plus-square">Add Item</i>
+    </button>
+  </div>
+</nav>
