2bec9aba1ef1c9ca98676b6656a1103cc3558266
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-10-07 17:06:26 -0700

    Adding transitions for elements. Working on fixing a problem with one of the transitions.

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -1,5 +1,6 @@
 <script>
   import { getContext } from "svelte";
+  import { slide } from "svelte/transition";
 
   export let id;
   export let name = "";
@@ -24,11 +25,16 @@
       >
     </h2>
     {#if displayAmount}
-      <h4>Amount: ${amount}</h4>
+      <h4 transition:slide>
+        Amount: ${amount}
+      </h4>
     {/if}
   </div>
   <div class="expense-buttons">
-    <button class="expense-btn edit-btn" on:click={() => setModifiedExpense(id)}>
+    <button
+      class="expense-btn edit-btn"
+      on:click={() => setModifiedExpense(id)}
+    >
       <i class="fas fa-pen" />
     </button>
     <button class="expense-btn delete-btn" on:click={() => removeExpense(id)}>
--- src/ExpenseList.svelte
+++ src/ExpenseList.svelte
@@ -1,16 +1,19 @@
 <script>
+  import { fly } from "svelte/transition";
+
   import SectionTitle from "./Title.svelte";
   import Expense from "./Expense.svelte";
 
   export let expenses = [];
-  
 </script>
 
 <section>
   <SectionTitle title="Expense List" />
   <ul>
-    {#each expenses as expense}
-      <Expense {...expense} />
+    {#each expenses as expense, index}
+      <div transition:fly={{ x: 200, delay: (index + 1) * 700 }}>
+        <Expense {...expense} />
+      </div>
     {:else}
       <h2>Currently there are no expenses!</h2>
     {/each}
